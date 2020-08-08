953fb870b39273b0e48622a7d2146d7bf874e012
Octomato <octomato@outlook.com>
2020-08-08 11:05:57 +0800

    import dependencies.BuildPlugins at build.gradle

---
+++ buildSrc/src/main/kotlin/dependencies/BuildPlugins.kt
@@ -0,0 +1,11 @@
+package dependencies
+
+/**
+ * Manage of all gradle build plugins
+ */
+object BuildPlugins {
+    const val ANDROID_APPLICATION = "com.android.application"
+    const val ANDROID_LIBRARY = "com.android.library"
+    const val KOTLIN_ANDROID = "kotlin-android"
+    const val KOTLIN_ANDROID_EXTENSIONS = "kotlin-android-extensions"
+}
